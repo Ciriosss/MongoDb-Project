192b6e0b25b97148602299526a29734d0aa9793f
Domenico Ciriello <domenicociriello2000@gmail.com>
2021-03-16 23:22:06 +0100

    profit

--- app/views.py
+++ app/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-from .models import SellOrder, BuyOrder
+from .models import SellOrder, BuyOrder, Profile
 
 # Create your views here.
 def home(request):
     return render(request,'app/home.html', {})
 
+
 def t_balance(request):
-    return render(request, 'app/t_balance.html', {})
+    profiles = Profile.objects.all()
+    if request.GET.get('profile'):
+        profile = request.GET.get('profile')
+        revenues = 0
+        costs = 0
+        profit = 0
+        sellorders = SellOrder.objects.filter(profile=profile)
+        for sellorder in sellorders:
+            revenues += ((sellorder.quantity * sellorder.price) - (sellorder.remaining * sellorder.price))
+        buyorders = BuyOrder.objects.filter(profile=profile)
+        for buyorder in buyorders:
+            costs += ((buyorder.quantity * buyorder.price) - (buyorder.remaining * buyorder.price))
+        profit = (revenues - costs)
+    else:
+        profit = 0
+    return render(request, 'app/t_balance.html', {'profiles' : profiles, 'profit' : profit})
+
+
+
 
 def activeOrders(request):
     sellorders  = SellOrder.objects.all().filter(matched = False).order_by('-datetime')
